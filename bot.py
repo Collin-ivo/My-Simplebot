ebb41203b3b6688a82a72cd68afcfbee80d28f83
Collin-ivo <n_13@bk.ru>
2018-11-29 00:54:02 +0300

    bot 1 commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import requests
+import urllib3
+
+from telegram.ext import Updater, CommandHandler
+
+TOKEN='748030895:AAGshtffnUAGrFKUBI-rOOA3fKrxfoVldtw'
+# REQUEST_KWARGS={
+#     'proxy_url': 'https://190.151.10.226:8080/',
+#     # Optional, if you need authentication:
+# }
+
+def start_bot(bot, updtr):
+    print('Вызван /start')
+    mytext = """Привет пользователь!
+
+    Я простой бот и понимаю только одну комманду /start
+    """
+    updtr.message.reply_text(mytext)
+
+def main():
+    updater = Updater(TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start_bot))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
