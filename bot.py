6b6f3a435f94dc9bdae6c4618d1e134ded0eab58
Nik_Ivanov_outdoor <n_13@bk.ru>
2018-11-29 19:01:45 +0000

    Bot-1

--- bot.py
+++ bot.py
@@ -2,30 +2,40 @@
 # -*- coding: utf-8 -*-
 import requests
 import urllib3
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
 
-from telegram.ext import Updater, CommandHandler
+import logging
 
-TOKEN='748030895:AAGshtffnUAGrFKUBI-rOOA3fKrxfoVldtw'
-# REQUEST_KWARGS={
-#     'proxy_url': 'https://190.151.10.226:8080/',
-#     # Optional, if you need authentication:
-# }
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
-def start_bot(bot, updtr):
-    print('Вызван /start')
-    mytext = """Привет пользователь!!!!
 
-    Я простой бот и понимаю только одну комманду /start
-    """
-    updtr.message.reply_text(mytext)
+TOKEN = '748030895:AAGshtffnUAGrFKUBI-rOOA3fKrxfoVldtw'
+
+
+def start_bot(bot, update):
+    mytext = "Привет {}!!!! Я простой бот и понимаю только одну комманду {}".format(update.message.chat.first_name, '/start')
+    update.message.reply_text(mytext)
+
+def chat(bot, update):
+    text = update.message.text
+    logging.info(text)
+    update.message.reply_text(text)
 
 def main():
     updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, chat))
 
     updater.start_polling()
     updater.idle()
 
 if __name__ == "__main__":
+    logging.info("Bot started")
     main()
